fcecbdfa803ea5aa856cf50a72fd02515969b37f
sudhanshu <sudhanshuraj8917@gmail.com>
2020-09-03 09:59:49 -0700

    initial commit

---
+++ cities/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import TemplateView, ListView
+
+from .models import City
+
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class SearchResultsView(ListView):
+    model = City
+    template_name = 'search_results.html'
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        object_list = City.objects.filter(
+            name__exact=query
+        )
+        return object_list
